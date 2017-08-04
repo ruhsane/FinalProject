78d140958b56abcb782c16f50ed11e4a6a39abcb
Jose <demouser@MacBook-Air.local>
2017-08-04 07:28:04 -0400

    no message

--- main.py
+++ main.py
@@ -43,8 +43,7 @@ class MainHandler(webapp2.RequestHandler):
             greeting = ('<a href="%s">Sign In</a>' % users.create_login_url('/'))
             listSavedEvents = ('')
         login = {
-                "Signin" : greeting,
-                "listOfSavedEvents" : listSavedEvents
+                "Signin" : greeting
                 }
         self.response.write(template.render(login))
 
@@ -168,8 +167,6 @@ class EventInfo(webapp2.RequestHandler):
 
             AMorPM = ""
             minute = start_time_time[1]
-            print "==================="
-            print start_time_time[0]
             if int(start_time_time[0]) >= 12:
                 AMorPM = "PM"
             else:
@@ -177,8 +174,6 @@ class EventInfo(webapp2.RequestHandler):
             hour = int(start_time_time[0]) % 12
             if hour == 0:
                 hour = 12
-            print "======================="
-            print AMorPM
             finalTime = str(hour) + ":" + str(minute) + " " + AMorPM
 
             event_start_time = finalDate + finalTime
@@ -223,7 +218,6 @@ class EventInfo(webapp2.RequestHandler):
 class ResultsHandler(webapp2.RequestHandler):
     def get(self):
         if not self.request.get("location") and not self.request.get("category"):
-            print "asdf"
             self.response.write('Error: Please go back and pick a location and category<form method="get" action="/"><button id=backbutton type="submit">Back</button></form>')
         else:
             template = jinja_environment.get_template('templates/results.html')
@@ -242,21 +236,10 @@ class ResultsHandler(webapp2.RequestHandler):
             event_json_content = event_data_source.read()
             parsed_event_dictionary = json.loads(event_json_content)
             event_dictionary = {}
+            print parsed_event_dictionary
             if parsed_event_dictionary['events'] is not None:
                 listOfEvents = parsed_event_dictionary["events"]["event"]
-            else:
-                template = jinja_environment.get_template('templates/results.html')
-                base_url = "http://api.eventful.com/json/events/search?app_key=dTJDKdL9vWFkMrwQ&page_size=30"
-                #remember to add code to make more than 10 events &page_size=100
-                url = base_url + "&location=" + str(self.request.get("location")) + "&category=" +str(self.request.get("category"))
-                event_data_source= urllib2.urlopen(url)
-                event_json_content = event_data_source.read()
-                parsed_event_dictionary = json.loads(event_json_content)
-                event_dictionary = {}
-                if parsed_event_dictionary['events'] is not None:
-                    listOfEvents = parsed_event_dictionary["events"]["event"]
-                else:
-                    listOfEvents = []
+                print "hola"
                 i = 0
                 event_title_list = []
                 event_id_list = []
@@ -391,100 +374,29 @@ class ResultsHandler(webapp2.RequestHandler):
                         elif self.request.get('category') == '':
                             event_image_url_medium[listOfEvents[i]["title"]] = "/resources/other_image.png"
                     else:
-                        AMorPM = "AM"
-                    hour = int(stop_time_time[0]) % 12
-                    if hour == 0:
-                        hour = 12
-                    finalTime = str(hour) + ":" + str(minute) + " " + AMorPM
-
-                    event_stop_time = finalDate + finalTime
-                    event_stop_time_id[listOfEvents[i]["title"]] = event_stop_time
-                if listOfEvents[i]["image"]is None:
-                    if self.request.get('category') == 'outdoors_recreation':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/outdoors_image.jpg"
-                    elif self.request.get('category') == 'music':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/concerts_image.gif"
-                    elif self.request.get('category') == 'art':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/art_image.png"
-                    elif self.request.get('category') == 'animals':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/pets_image.jpg"
-                    elif self.request.get('category') == 'sports':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/sports_image.png"
-                    elif self.request.get('category') == 'comedy':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/comedy_image.jpg"
-                    elif self.request.get('category') == 'family_fun_kids':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/family_image.jpg"
-                    elif self.request.get('category') == 'singles_social':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/nightlife_image.jpg"
-                    elif self.request.get('category') == 'performing_arts':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/performing_arts_image.jpg"
-                    elif self.request.get('category') == 'learning_education':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/educational_image.jpg"
-                    elif self.request.get('category') == 'movies_film':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/movies_image.jpg"
-                    elif self.request.get('category') == 'food':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/food_image.jpg"
-                    elif self.request.get('category') == 'support':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/health_image.jpg"
-                    elif self.request.get('category') == 'attractions':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/museums_image.jpg"
-                    elif self.request.get('category') == 'politics_activism':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/politics_image.jpg"
-                    elif self.request.get('category') == 'science':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/science_image.jpg"
-                    elif self.request.get('category') == 'technology':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/technology_image.jpg"
-                    elif self.request.get('category') == 'conference':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/conferences_image.jpg"
-                    elif self.request.get('category') == 'fundraisers':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/fundraiser_image.jpg"
-                    elif self.request.get('category') == 'holiday':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/holiday_image.jpg"
-                    elif self.request.get('category') == 'community':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/community_image.jpg"
-                    elif self.request.get('category') == 'books':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/books_image.jpg"
-                    elif self.request.get('category') == 'business':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/business_image.jpg"
-                    elif self.request.get('category') == 'schools_alumni':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/school_events.jpg"
-                    elif self.request.get('category') == 'clubs_association':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/organizations_image.jpg"
-                    elif self.request.get('category') == 'sales':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/sales_image.jpg"
-                    elif self.request.get('category') == 'religion_spirituality':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/religion_image.jpg"
-                    elif self.request.get('category') == 'none':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/none_image.jpg"
-                    elif self.request.get('category') == 'other':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/other_image.png"
-                    elif self.request.get('category') == '':
-                        event_image_url_medium[listOfEvents[i]["title"]] = "/resources/other_image.png"
-                else:
-                    if type(listOfEvents[i]["image"]) is list:
-                        event_image_url_medium[listOfEvents[i]["title"]] = (listOfEvents[i]["image"][0]["medium"]["url"]).replace("medium","large")
-                    else:
-                        event_image_url_medium[listOfEvents[i]["title"]] = (listOfEvents[i]["image"]["medium"]["url"]).replace("medium","large")
-                event_title_venue[listOfEvents[i]["title"]] = listOfEvents[i]["venue_name"]
-                event_category[listOfEvents[i]["title"]] = str(self.request.get("category"))
-                i += 1
-            event_dictionary = {
-                "eventTitlePhotoUrl": event_image_url_medium,
-                "eventTitleVenue": event_title_venue,
-                "eventTitles": event_title_list,
-                "eventIds": event_id_list,
-                "eventTitleId" : event_title_id,
-                "eventCategory" : event_category,
-                "eventStartTimeId": event_start_time_id,
-                "eventStopTimeId": event_stop_time_id,
-                "Signin" : signin
-
-                }
-            if event_dictionary["eventTitles"] == []:
+                        if type(listOfEvents[i]["image"]) is list:
+                            event_image_url_medium[listOfEvents[i]["title"]] = (listOfEvents[i]["image"][0]["medium"]["url"]).replace("medium","large")
+                        else:
+                            event_image_url_medium[listOfEvents[i]["title"]] = (listOfEvents[i]["image"]["medium"]["url"]).replace("medium","large")
+                    event_title_venue[listOfEvents[i]["title"]] = listOfEvents[i]["venue_name"]
+                    event_category[listOfEvents[i]["title"]] = str(self.request.get("category"))
+                    i += 1
+                event_dictionary = {
+                    "eventTitlePhotoUrl": event_image_url_medium,
+                    "eventTitleVenue": event_title_venue,
+                    "eventTitles": event_title_list,
+                    "eventIds": event_id_list,
+                    "eventTitleId" : event_title_id,
+                    "eventCategory" : event_category,
+                    "eventStartTimeId": event_start_time_id,
+                    "eventStopTimeId": event_stop_time_id,
+                    "Signin" : signin
+                    }
+            else:
                 event_dictionary["error"] = "Sorry! There are no events for this category in this location"
             self.response.write(template.render(event_dictionary))
 
-    class EventInfo(webapp2.RequestHandler):
+class EventInfo(webapp2.RequestHandler):
         def get(self):
             template = jinja_environment.get_template('templates/event_specifics.html')
             user = users.get_current_user()
@@ -602,8 +514,6 @@ class ResultsHandler(webapp2.RequestHandler):
 
                 AMorPM = ""
                 minute = start_time_time[1]
-                print "==================="
-                print start_time_time[0]
                 if int(start_time_time[0]) >= 12:
                     AMorPM = "PM"
                 else:
